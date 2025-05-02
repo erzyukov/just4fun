23abd4b1effa360ec464e623f89eda8ad55c5082
Erzyukov Alexey <erzyukov@gmail.com>
2025-05-02 15:33:52 +1000

    input manager

---
+++ Assets/Game/Scripts/Managers/InputManager.cs
@@ -0,0 +1,32 @@
+﻿namespace Game.Managers
+{
+ 	using Zenject;
+
+
+	public interface IInputManager
+	{
+		Controls.CameraActions			Camera			{get;}
+	}
+
+	public class InputManager : IInputManager, IInitializable
+	{
+		Controls _controls;
+
+
+		public void Initialize()
+		{
+			_controls		= new Controls();
+
+			Camera			= _controls.Camera;
+
+			Camera			.Enable();
+		}
+
+#region IInputSystemManager
+
+		public Controls.CameraActions		Camera				{ get; private set; }
+
+#endregion
+    
+	}
+}
